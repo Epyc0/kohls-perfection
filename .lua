90d296b02c28056dd04a975e01d10d35cd5d311e
Harkad <56307996+Harkad@users.noreply.github.com>
2019-10-08 16:14:53 +0200

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,21 @@
+--boulevard auto farm
+
+for i = 1, 1 do
+
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(88.4571915, 174.523834, 195.584885)
+
+wait(1)
+	
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(229.2611339, 175.0088591, 300.6790047)
+wait(0.1)
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(226.848068, 175.0088591, 373.2665991)
+wait(0.1)
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(65.80896, 174.670258, 284.148926)
+wait(0.1)
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(65.6980591, 175.0088591, 368.854431)
+wait(0.1)
+game:service'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(88.4571915, 174.523834, 195.584885)
+
+wait(2)
+
+end
